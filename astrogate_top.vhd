d1d0d04e40977f8f70dc6e119e61938fb0cb5fc1
Liam Davies <mangobox228@hotmail.com.au>
2025-09-02 11:56:06 +1000

    Finished first version of VGA output

--- astrogate_top.vhd
+++ astrogate_top.vhd
@@ -12,26 +12,15 @@ entity astrogate_top is
          vga_green : out std_logic;
          vga_blue  : out std_logic;
          vga_hsync : out std_logic;
-         vga_vsync : out std_logic
+         vga_vsync : out std_logic;
+         -- Camera SCCB
+         sccb_clk_out : out std_logic
        );
 end astrogate_top;
 
 architecture rtl of astrogate_top is
 begin
-  -- p_clock_proc: process(clk)
-  -- begin
-  --   if rising_edge(clk) then
-  --     if rst = '1' then
-  --       -- Global Reset 
-  --       red <= clk;
-  --     else
-  --       
-  --     end if;
-  --   end if;
-  -- end process p_clock_proc;
-
-
--- Instantiate VGA output
+  -- Instantiate VGA output
   e_vga_output : entity work.VGA
     port map (
       i_clk50 => clk,
@@ -41,4 +30,10 @@ begin
       o_hs => vga_hsync,
       o_vs => vga_vsync
    );
+
+  e_camera_sccb : entity work.camera_sccb
+    port map (
+      clk_in => clk,
+      sccb_clk_out => sccb_clk_out
+     );
 end rtl;
